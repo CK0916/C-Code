05612798e12e5741839c94f850bcbc85f007f7f6
CK0916 <wzheng@sia.cn>
2018-11-01 10:52:13 +0800

    the second cmake test

--- cmake_test/CMakeFiles/Demo.dir/DependInfo.cmake
+++ cmake_test/CMakeFiles/Demo.dir/DependInfo.cmake
@@ -14,6 +14,7 @@ set(CMAKE_CXX_TARGET_INCLUDE_PATH
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/wangzheng/C-Code/cmake_test/math/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
